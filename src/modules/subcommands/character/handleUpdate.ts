a41b2a4fedd5c869d580c8a565b7977f8fcc8d1d
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-09-29 19:26:43 -0700

    feat: character commands

---
+++ src/modules/subcommands/character/handleUpdate.ts
@@ -0,0 +1,53 @@
+/* eslint-disable jsdoc/require-param */
+import CharacterModel from "../../../database/models/CharacterModel";
+import { CommandHandler } from "../../../interfaces/CommandHandler";
+import { errorEmbedGenerator } from "../../../utils/errorEmbedGenerator";
+import { rosaErrorHandler } from "../../../utils/rosaErrorHandler";
+import { viewCharacter } from "../../viewCharacter";
+
+/**
+ * Handles the logic for updating a character.
+ */
+export const handleUpdate: CommandHandler = async (Rosa, interaction) => {
+  try {
+    const user = interaction.user.id;
+
+    const character = await CharacterModel.findOne({ discordId: user });
+
+    if (!character) {
+      await interaction.editReply({
+        content: "You have not created a character yet!",
+      });
+      return;
+    }
+
+    const name = interaction.options.getString("name");
+    const gender = interaction.options.getString("gender");
+    const race = interaction.options.getString("race");
+    const description = interaction.options.getString("description");
+
+    character.name = name || character.name;
+    character.gender = gender || character.gender;
+    character.race = race || character.race;
+    character.description = description || character.description;
+
+    await character.save();
+
+    const characterEmbed = viewCharacter(character);
+
+    await interaction.editReply({
+      content: "Your character has been created!",
+      embeds: [characterEmbed],
+    });
+  } catch (error) {
+    const errorId = await rosaErrorHandler(
+      Rosa,
+      "create command",
+      error,
+      interaction.guild?.name
+    );
+    await interaction.editReply({
+      embeds: [errorEmbedGenerator(Rosa, "create", errorId)],
+    });
+  }
+};
---
+++ src/modules/viewCharacter.ts
@@ -0,0 +1,23 @@
+import { MessageEmbed } from "discord.js";
+
+import { Character } from "../interfaces/game/Character";
+
+/**
+ * Generates a message embed to display the information for a character.
+ *
+ * @param {Character} character The character to display information for.
+ * @returns {MessageEmbed} The generated embed.
+ */
+export const viewCharacter = (character: Character): MessageEmbed => {
+  const { name, description, race, gender, stats } = character;
+  const embed = new MessageEmbed();
+  embed.setTitle(`${name}, the ${gender} ${race}`);
+  embed.setDescription(description);
+  embed.addField("Attack", stats.attack.toString(), true);
+  embed.addField("Defence", stats.defence.toString(), true);
+  embed.addField("Magic", stats.magic.toString(), true);
+  embed.addField("Health", stats.health.toString(), true);
+  embed.addField("Mana", stats.mana.toString(), true);
+
+  return embed;
+};
