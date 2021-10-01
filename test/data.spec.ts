8a15a3a705e96d2b9b94787120abffa726e35be5
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-09-30 20:35:54 -0700

    chore(tools): test uniqueness

--- test/data.spec.ts
+++ test/data.spec.ts
@@ -47,4 +47,79 @@ suite("Data", () => {
       });
     }
   });
+
+  suite("Unique Validation", () => {
+    test("Equipment should be unique.", () => {
+      const equipSet = new Set();
+      for (const item of equippables) {
+        assert.isFalse(equipSet.has(item.name), `${item.name} is not unique.`);
+        equipSet.add(item.name);
+      }
+    });
+
+    test("Sellables should be unique.", () => {
+      const sellSet = new Set();
+      for (const item of sellables) {
+        assert.isFalse(sellSet.has(item.name), `${item.name} is not unique.`);
+        sellSet.add(item.name);
+      }
+    });
+
+    test("Consumables should be unique.", () => {
+      const consumSet = new Set();
+      for (const item of consumables) {
+        assert.isFalse(consumSet.has(item.name), `${item.name} is not unique.`);
+        consumSet.add(item.name);
+      }
+    });
+
+    test("Monsters should be unique.", () => {
+      const monsterSet = new Set();
+      for (const monster of monsters) {
+        assert.isFalse(
+          monsterSet.has(monster.name),
+          `${monster.name} is not unique.`
+        );
+        monsterSet.add(monster.name);
+      }
+    });
+
+    test("Explorations should be unique.", () => {
+      const expSet = new Set();
+      for (const exp of explorations) {
+        assert.isFalse(expSet.has(exp.name), `${exp.name} is not unique.`);
+        expSet.add(exp.name);
+      }
+    });
+
+    test("Dungeons should be unique.", () => {
+      const dungeonSet = new Set();
+      for (const dungeon of dungeons) {
+        assert.isFalse(
+          dungeonSet.has(dungeon.name),
+          `${dungeon.name} is not unique.`
+        );
+        dungeonSet.add(dungeon.name);
+      }
+    });
+
+    test("All items should be unique.", () => {
+      const itemSet = new Set();
+      for (const item of items) {
+        assert.isFalse(itemSet.has(item.name), `${item.name} is not unique.`);
+        itemSet.add(item.name);
+      }
+    });
+
+    test("Adventures should be unique.", () => {
+      const adventureSet = new Set();
+      for (const adventure of adventures) {
+        assert.isFalse(
+          adventureSet.has(adventure.name),
+          `${adventure.name} is not unique.`
+        );
+        adventureSet.add(adventure.name);
+      }
+    });
+  });
 });
