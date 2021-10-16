fcd9ce7efaeee4961e0bd728691758f3823e8f2d
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-10-15 21:25:18 -0700

    feat: item data

--- src/config/data/sellables.ts
+++ src/config/data/sellables.ts
@@ -25,4 +25,34 @@ export const sellables: Sellable[] = [
     description: "The silk from a spider.",
     value: 5,
   },
+  {
+    type: "sellable",
+    name: "Goblin Horn",
+    description: "The horn from a goblin.",
+    value: 7,
+  },
+  {
+    type: "sellable",
+    name: "Fairy Dust",
+    description: "Magical powder. You can't use it in this form.",
+    value: 10,
+  },
+  {
+    type: "sellable",
+    name: "Treant Root",
+    description: "A thick root.",
+    value: 15,
+  },
+  {
+    type: "sellable",
+    name: "Bark Strips",
+    description: "A bunch of tough bark.",
+    value: 10,
+  },
+  {
+    type: "sellable",
+    name: "Mandrake Root",
+    description: "A soft, sappy root",
+    value: 15,
+  },
 ];
