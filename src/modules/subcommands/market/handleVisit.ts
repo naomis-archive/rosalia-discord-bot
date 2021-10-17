38124ac99d8b2934f1c728479ce72093fbc52229
Siddhant <siddhantkhisty@gmail.com>
2021-10-18 04:33:20 +0530

    Added prettier formatting

--- src/modules/subcommands/market/handleVisit.ts
+++ src/modules/subcommands/market/handleVisit.ts
@@ -57,4 +57,3 @@ export const handleVisit: CommandHandler = async (Rosa, interaction) => {
 function getRandomValue(description: string[] | undefined): string {
   throw new Error("Function not implemented.");
 }
-
