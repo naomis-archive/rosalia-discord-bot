dd06296d6f5c6cefd68d9b868e1d70c3c6c27705
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-10-27 22:15:23 -0700

    feat: art command

---
+++ src/modules/subcommands/rosalia/handleArt.ts
@@ -0,0 +1,42 @@
+/* eslint-disable jsdoc/require-param */
+import { MessageEmbed } from "discord.js";
+
+import { artList } from "../../../config/artList";
+import { CommandHandler } from "../../../interfaces/CommandHandler";
+import { errorEmbedGenerator } from "../../../utils/errorEmbedGenerator";
+import { getCounts } from "../../../utils/getCounts";
+import { getRandomValue } from "../../../utils/getRandomValue";
+import { rosaErrorHandler } from "../../../utils/rosaErrorHandler";
+
+/**
+ * Returns information about the bot.
+ */
+export const handleArt: CommandHandler = async (Rosa, interaction) => {
+  try {
+    const art = getRandomValue(artList);
+    const artEmbed = new MessageEmbed();
+    artEmbed.setTitle(art.artName);
+    artEmbed.setDescription(
+      `This portrait of me was done by [${art.artist}](${art.artistUrl})`
+    );
+    artEmbed.setImage(
+      `https://www.rosalianightsong.com/assets/art/${art.fileName.replace(
+        /\s/g,
+        "%20"
+      )}`
+    );
+    artEmbed.setFooter("Would you like to make an art too?");
+
+    await interaction.editReply({ embeds: [artEmbed] });
+  } catch (err) {
+    const errorId = await rosaErrorHandler(
+      Rosa,
+      "art command",
+      err,
+      interaction.guild?.name
+    );
+    await interaction.editReply({
+      embeds: [errorEmbedGenerator(Rosa, "art", errorId)],
+    });
+  }
+};
