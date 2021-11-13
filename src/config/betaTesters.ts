9753c5c7dbacc8cead6b61d2df37dd8aaa2f15da
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-11-12 16:46:56 -0800

    feat: beta command

---
+++ src/config/betaTesters.ts
@@ -0,0 +1,14 @@
+export const betaTesters = [
+  "Skyhero#8088",
+  "Krish Gupta#6241",
+  "Norioman#6095",
+  "ieahleen#4468",
+  "AngelGirl2118#3677",
+  "minhlpham#3695",
+  "Hitz#5560",
+  "Sonichigo#1765",
+  "Sanju's Robotics Studio#0571",
+  "Ooigle#0114",
+  "Vyvy-vi#5040",
+  "Pradumnasaraf#7988",
+];
