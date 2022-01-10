3e73e5d2f9ad0ef555d2e39cb7d4056cf2780dda
Nicholas Carrigan (he/him) <nhcarrigan@gmail.com>
2022-01-09 18:28:42 -0800

    release: v1.2.0 (#228)
    * chore: changelog
    
    * 1.2.0

--- src/config/data/updates.ts
+++ src/config/data/updates.ts
@@ -1,3 +1,8 @@
-export const updateList = ["- Nothing here yet!"];
+export const updateList = [
+  "- Added link buttons to some of the embeds",
+  "- Added a `/rosalia contact` command to provide links to get in touch with us.",
+  "- Sorted the subcommands to make navigating the menu easier.",
+  "- Added a `/rosalia updates` command (this command!) to provide the latest changelog.",
+];
 
 export const nextUpdate = "<t:1642262400:F>";
