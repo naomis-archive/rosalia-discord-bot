e065528f82c485c1b005fbb257840d92282854b1
Naomi Carrigan <nhcarrigan@gmail.com>
2022-04-30 07:48:20 -0700

    release: v1.2.1 (#323)
    * chore: changelog
    
    * 1.2.1

--- src/config/data/updates.ts
+++ src/config/data/updates.ts
@@ -1,8 +1,5 @@
 export const updateList = [
-  "- Added link buttons to some of the embeds",
-  "- Added a `/rosalia contact` command to provide links to get in touch with us.",
-  "- Sorted the subcommands to make navigating the menu easier.",
-  "- Added a `/rosalia updates` command (this command!) to provide the latest changelog.",
+  "- Patched an error in the `/rosalia about` command.",
 ];
 
-export const nextUpdate = "<t:1642262400:F>";
+export const nextUpdate = "<t:1651935600:F>";
